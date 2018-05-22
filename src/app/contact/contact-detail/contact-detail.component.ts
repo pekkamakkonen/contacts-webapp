947040a4b9decde057d392f078b6ceb14a14db85
pekkamakkonen <pek.makkonen@gmail.com>
2018-05-22 14:11:56 +0300

    Refactored ContactDetail component to enable separating edit (update) and create cases.

--- src/app/contact/contact-detail/contact-detail.component.ts
+++ src/app/contact/contact-detail/contact-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ContactService} from '../services/contact.service';
 import {Contact} from '../contact';
@@ -14,33 +14,40 @@ import {ToolbarAction} from '../../ui/tool-bar/toolbar-action';
 export class ContactDetailComponent implements OnInit {
 
   contact: Contact;
+  editingEnabled: boolean;
 
   constructor(private router: Router, private route: ActivatedRoute,
               private contactService: ContactService, private toolbar: ToolbarService) {
     this.contact = new Contact();
+    this.editingEnabled = false;
   }
 
   ngOnInit() {
-    this.toolbar.toolbarOptions.next(
-      new ToolbarOptions(
-        'Contact', [
-          new ToolbarAction(this.onEdit, 'edit')
-        ]));
 
     const contactId = this.route.snapshot.paramMap.get('id');
+    let toolbarActions: ToolbarAction[];
 
     if (contactId == null) {
-      return;
+      // Create contact
+      this.editingEnabled = true;
+      toolbarActions = [];
+    } else {
+      // View/Edit contact
+      toolbarActions = [new ToolbarAction(this.onEdit.bind(this), 'edit')];
+
+      this.contactService.getContactByID(contactId).subscribe(response => {
+        this.contact = response;
+        console.log(this.contact);
+      }, error => {
+        console.error('Getting contact failed!');
+        console.error(error);
+        this.router.navigate(['/contacts']);
+      });
     }
 
-    this.contactService.getContactByID(contactId).subscribe(response => {
-      this.contact = response;
-      console.log(this.contact);
-    }, error => {
-      console.error('Getting contact failed!');
-      console.error(error);
-      this.router.navigate(['/contacts']);
-    });
+    this.toolbar.toolbarOptions.next(
+      new ToolbarOptions(
+        'Contact', toolbarActions));
   }
 
   onNavigateBack(): void {
@@ -52,7 +59,7 @@ export class ContactDetailComponent implements OnInit {
   }
 
   onEdit() {
-    console.log('TODO: activate/deactivate edit mode');
+    this.editingEnabled = !this.editingEnabled;
   }
 
 }
